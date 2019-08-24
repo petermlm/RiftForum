fb4f18e5899ba4a83cd83b6ab4a526c69f48cc24
Pedro Melgueira <pedromelgueira@gmail.com>
2019-08-24 18:03:04 +0200

    Exposes a few ports on prod

--- run-prod.sh
+++ run-prod.sh
@@ -3,4 +3,5 @@
 export UID=$(id -u)
 export GID=$(id -g)
 cd docker
-docker-compose -f docker-compose-prod.yml $@
+docker-compose -f docker-compose-prod.yml build
+docker-compose -f docker-compose-prod.yml up -d
